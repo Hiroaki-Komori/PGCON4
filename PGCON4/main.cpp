7b80b61ec71291bff460c24231e6ab44b1e26fe8
Takuo Doi <takuo.doi@iclound.com>
2014-08-26 02:21:53 +0900

    Removed unnecessary string copy.

--- PGCON4/main.cpp
+++ PGCON4/main.cpp
@@ -27,31 +27,21 @@ int main()
 	int inputNum;
 	cin >> inputNum;
 
-
 	for (int n = 0; n < inputNum; ++n)
 	{
 	    string input;
 		cin >> input;
 
-		int iSize = input.size();
-		int answer = iSize;
+		size_t iSize = input.length();
+        int answer = iSize;
 
 		string keyList(iSize, 0);//for listing each type of character.
-		string keyScan(iSize, 0);//for filtering the target key.
-		vector< vector<int> > indexList(iSize, vector<int>(iSize, 0));//list of each character's position.
-
 		int keyN = 0;
 		for (int i = 0; i < iSize; ++i)
 		{
 			char key = input[i];//pickup one char from the input.
-			is_key is_key_func(key);
-			is_not_key is_not_key_func(key);
 			if (keyList.find(key) == string::npos)
 			{
-				keyScan = input;//input -> filter
-				replace_if(keyScan.begin(), keyScan.end(), is_not_key_func, 0);
-				replace_if(keyScan.begin(), keyScan.end(), is_key_func, 1);
-				copy(keyScan.begin(), keyScan.end(), indexList[keyN].begin());
 				keyList[keyN] = key;
 				++keyN;
 			}
@@ -63,7 +53,8 @@ int main()
 				int oddKey = 0;
 				for (int k = 0; k < keyN; ++k)
 				{
-					int num = count(indexList[k].begin() + j, indexList[k].begin() + j + segment + 1, 1);
+				    is_key is_key_func(keyList[k]);
+					int num = count_if(input.begin() + j, input.begin() + j + segment + 1, is_key_func);
 					is_odd(num) ? ++oddKey : 0;
 				}
 				(oddKey <= 1) ? ++answer : 0;
